df5c1c7e35cba8f438953b824b51306f5ddc27f8
Radzim Kowalow <radzim_ko@wp.pl>
2020-07-01 06:27:31 +0200

    initial project structure

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # data-science-quizzes
-Repository containing awesome data science quizzes/examples open for everybody!
+
+> Website with awesome quizzes more or less related to data science
+
+## Development
+
+```bash
+yarn dev
+yarn build
+```
+
+For more details, please head VuePress's [documentation](https://v1.vuepress.vuejs.org/).
+
---
+++ docs/.vuepress/components/Answer.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="q-container" v-bind:class="checked ? cls : ''">
+    <input type="checkbox" v-model="checked" />
+    <label>
+      <slot></slot>
+    </label>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    correct: {
+      type: String,
+      default: "false"
+    }
+  },
+  data() {
+    return {
+      checked: false,
+      cls: this.correct == "true" ? "green" : "red"
+    };
+  }
+};
+</script>
+
+<style scoped>
+.q-container {
+  display: flex;
+  align-items: center;
+}
+.q-container label {
+  margin-left: 2rem;
+}
+.q-container input[type="checkbox"] {
+  position: relative;
+  margin: 1rem;
+  padding: 1rem;
+}
+.green {
+  background: rgba(62, 175, 124, 0.4);
+}
+.red {
+  background: rgba(175, 62, 62, 0.4);
+}
+</style>

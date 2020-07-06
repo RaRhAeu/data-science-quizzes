57e96ab85f9a68ba17be29a97b1cd72241002694
Radzim Kowalow <radzim_ko@wp.pl>
2020-07-07 01:47:26 +0200

    wt100

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,12 @@
+
 #!/usr/bin/env sh
 
 set -e
 
 if [ -z "$ACCESS_TOKEN" ] && [ -z "$GITHUB_TOKEN" ]
 then
-  echo "You must provide the action with either a Personal Access Token or the GitHub Token secret in order to deploy."
-  exit 1
+ echo "You must provide the action with either a Personal Access Token or the GitHub Token secret in order to deploy."
+ exit 1
 fi
 
 REPOSITORY_PATH="https://${ACCESS_TOKEN:-"x-access-token:$GITHUB_TOKEN"}@github.com/${GITHUB_REPOSITORY}.git" && \
@@ -18,12 +19,12 @@ COMMIT_NAME=`jq '.pusher.name' ${GITHUB_EVENT_PATH}`
 # If the commit email/name is not found in the event payload then it falls back to the actor.
 if [ -z "$COMMIT_EMAIL" ]
 then
-  COMMIT_EMAIL="${GITHUB_ACTOR:-github-pages-deploy-action}@users.noreply.github.com"
+ COMMIT_EMAIL="${GITHUB_ACTOR:-github-pages-deploy-action}@users.noreply.github.com"
 fi
 
 if [ -z "$COMMIT_NAME" ]
 then
-  COMMIT_NAME="${GITHUB_ACTOR:-GitHub Pages Deploy Action}"
+ COMMIT_NAME="${GITHUB_ACTOR:-GitHub Pages Deploy Action}"
 fi
 
 # Directs the action to the the Github workspace.
@@ -37,7 +38,6 @@ git config --global user.name "${COMMIT_NAME}" && \
 
 export BASE_URL="/data-science-quizzes/"
 
-npm install -D
 npm run build
 
 cd docs/.vuepress/dist

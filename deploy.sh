f89c1fb6849023e11323325848d1c67a15ecac76
Radzim Kowalow <radzim_ko@wp.pl>
2020-07-06 15:46:09 +0200

    deploy script added

---
+++ deploy.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env sh
+
+set -e
+
+export BASE_URL="/data-science-quizzes/"
+
+npm run build
+
+cd docs/.vuepress/dist
+
+git init
+git add -A
+git commit -m "deploy"
+git push -f git@github.com:RaRhAeu/data-science-quizzes.git master:gh-pages
+
+unset BASE_URL
+
+cd -
